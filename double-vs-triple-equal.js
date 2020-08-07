30b496b221ffd18a651d25784158b74632f058cf
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-08-07 21:04:33 +0600

    double and triple equal check

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+const first = 2;
+const second = "2";
+if(first===second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is true");
+}
+
+// double equal only checks the value
+
+//triple equal checks the value and type also
