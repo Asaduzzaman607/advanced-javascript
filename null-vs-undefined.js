58f62d6e339add2181a07fd399836db44f29d23d
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-08-07 20:50:29 +0600

    ull vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+let pakhi;
+function add(num1,num2){
+    console.log(num1 + num2);
+    return
+
+
+}
+
+//If you don't pass parameter in right way you will get undefined
+
+function add(num1,num2){
+    console.log(num2);
+
+
+}
+
+const result = add(12)
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
+
+let ages=[8,12,13];
+console.log(ages.[11]);// 11th element is not pesent here 
+
+//null will be set when there is no element or when the element will be removed
